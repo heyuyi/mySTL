96b1d4381c35268ad18afaa1b936bd4bd6ac57c9
he yuyi <1244620185@qq.com>
2016-01-20 18:15:45 +0800

    2016-1-20

--- include/algorithm.h
+++ include/algorithm.h
@@ -2,7 +2,7 @@
 #ifndef _ALGORITHM_H_
 #define _ALGORITHM_H_
 
-#include <functional>
+//#include <functional>
 
 namespace mySTL {
 // some kinds of sort algorithm
@@ -94,15 +94,129 @@ template<typename _TIt> inline
 // quick-sort
 template<typename _TIt,
 	typename _FPtr> inline
-	void quickSort(_TIt beg, _TIt end, _FPtr func)
+	void _Quick_sort_0(_TIt beg, _TIt end, _FPtr func)
 	{
+		auto s = end - beg;
+		if (s > 1) {
+			auto val = *beg;
+			_TIt i = beg + 1, j = beg;
+			for (; i < end; ++i) {
+				if (func(*i, val)) {
+					++j;
+					swap(*j, *i);
+				}
+			}
+			if (j != beg)
+				swap(*beg, *j);
+			_Quick_sort_0(beg, j, func);
+			_Quick_sort_0(j + 1, end, func);
+		}
+	}
 
+template<typename _TIt,
+	typename _FPtr> inline
+	void _Quick_sort_1(_TIt beg, _TIt end, _FPtr func)
+	{
+		auto s = end - beg;
+		if (s > 1) {
+			auto val = *beg;
+			_TIt i = beg, j = end;
+			for (;;) {
+				do {
+					++i;
+				} while (i < end && func(*i, val));
+				do {
+					--j;
+				} while (/*j >= beg && */func(val, *j));
+				if (i < j)
+					swap(*i, *j);
+				else
+					break;
+			}
+			swap(*beg, *j);
+			_Quick_sort_1(beg, j, func);
+			_Quick_sort_1(j + 1, end, func);
+		}
+	}
+
+template<typename _TIt,
+	typename _FPtr> inline
+	typename _TIt mid3(_TIt a1, _TIt a2, _TIt a3, _FPtr func)
+	{
+		if (func(*a1, *a2)) {
+			if (func(*a2, *a3))
+				return a2;
+			else if (func(*a1, *a3))
+				return a3;
+			else
+				return a1;
+		}
+		else {
+			if (func(*a1, *a3))
+				return a1;
+			else if (func(*a2, *a3))
+				return a3;
+			else
+				return a2;
+		}
+	}
+
+template<typename _TIt,
+	typename _FPtr> inline
+	void _Quick_sort_2(_TIt beg, _TIt end, _FPtr func)
+	{
+		auto len = end - beg;
+		if (len > 1) {
+			if (len > 7) {
+				_TIt l = beg;
+				_TIt m = beg + len / 2;
+				_TIt n = end - 1;
+				if (len > 40) {
+					auto d = len / 8;
+					l = mid3(l, l + d, l + 2 * d, func);
+					m = mid3(m - d, m, m + d, func);
+					n = mid3(n - 2 * d, n - d, n, func);
+				}
+				_TIt it = mid3(l, m, n, func);
+				swap(*beg, *it);
+			}			
+
+			auto val = *beg;
+			_TIt i = beg, j = end;
+			for (;;) {
+				do {
+					++i;
+				} while (i < end && func(*i, val));
+				do {
+					--j;
+				} while (/*j >= beg && */func(val, *j));
+				if (i < j)
+					swap(*i, *j);
+				else
+					break;
+			}
+			swap(*beg, *j);
+			_Quick_sort_2(beg, j, func);
+			_Quick_sort_2(j + 1, end, func);
+		}
+	}
+
+template<typename _TIt,
+	typename _FPtr> inline
+	void quickSort(_TIt beg, _TIt end, _FPtr func, int n = 1)
+	{
+		/*if(n == 0)
+			_Quick_sort_0(beg, end, func);
+		else if (n == 1)
+			_Quick_sort_1(beg, end, func);
+		else*/
+			_Quick_sort_2(beg, end, func);
 	}
 
 template<typename _TIt> inline
-	void quickSort(_TIt beg, _TIt end)
+	void quickSort(_TIt beg, _TIt end, int n = 1)
 	{
-		quickSort(beg, end, std::less<>());
+		quickSort(beg, end, std::less<>(), n);
 	}
 }
 #endif
--- sortTest/sortTest.cpp
+++ sortTest/sortTest.cpp
@@ -4,16 +4,46 @@
 #include "stdafx.h"
 #include "..\include\algorithm.h"
 #include <vector>
+#include <algorithm>
+#include <ctime>
+#include <random>
+#include <iostream>
 
 using namespace std;
 using namespace mySTL;
 
 int main()
 {
-	vector<int> a;
-	for (int i = 100; i > 0; --i)
-		a.push_back(i);
-	heapSort(a.begin(), a.end());
+	default_random_engine e(time(0));
+	uniform_int_distribution<> u;
+	vector<int> a1, a2, a3, a4, a5, a6;
+	clock_t start, end;
+
+	for (int i = 10000; i > 0; --i)
+		a1.push_back(u(e));
+//	for (int i = 10000; i > 0; --i)
+//		a1.push_back(i);
+	a2 = a1; a3 = a1; a4 = a1; a5 = a1; a6 = a1;
+
+/*
+//	i =								10000		|	
+	sort(a1.begin(), a1.end());			// 20ms		|
+//	insertSort(a2.begin(), a2.end());	// 57s		|
+//	heapSort(a3.begin(), a3.end());		// 710ms	|
+	quickSort(a4.begin(), a4.end(), 0);	//159ms		|
+	quickSort(a5.begin(), a5.end(), 1);	//128ms		|
+	quickSort(a6.begin(), a6.end(), 2);
+	*/
+
+	cout << "data size: i = " << a1.size() << endl;
+	start = clock();
+	sort(a1.begin(), a1.end());
+	end = clock();
+	cout << "STL sort: " << end - start << endl;
+	start = clock();
+	quickSort(a4.begin(), a4.end(), 2);
+	end = clock();
+	cout << "quick sort.2: " << end - start << endl;
 	return 0;
 }
 
