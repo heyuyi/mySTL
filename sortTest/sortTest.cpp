a7b96477ff3834bad9ce2391a4c5ca5636d82807
he yuyi <1244620185@qq.com>
2016-01-20 09:44:51 +0800

    2016-1-20

---
+++ sortTest/sortTest.cpp
@@ -0,0 +1,19 @@
+// sortTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include "..\include\algorithm.h"
+#include <vector>
+
+using namespace std;
+using namespace mySTL;
+
+int main()
+{
+	vector<int> a;
+	for (int i = 100; i > 0; --i)
+		a.push_back(i);
+	heapSort(a.begin(), a.end());
+	return 0;
+}
+
